dd67043684b07f30c64a6a66631088e1a716bc8f
Vaishnavi Kalhapure <vaishnavi02kalhapure@gmail.com>
2025-11-14 15:09:40 +0530

    api route to get all my reviews

--- Server/routes/review.js
+++ Server/routes/review.js
@@ -5,16 +5,27 @@ const pool = require('../db/db');
 const result = require('../utils/result')
 
 
-router.post('/:movieId',(req,res)=>{
+router.post('/:movieId', (req, res) => {
     const { movieId } = req.params
-    const { review,rating,userId }= req.body;
+    const { review, rating, userId } = req.body;
 
-    const sql= 'INSERT INTO reviews(review,rating,user_id,movie_id) VALUES (?,?,?,?)'
-    pool.query(sql,[review,rating,userId,movieId],(error,data)=>{
-         res.send(result.createResult(error,data));
+    const sql = 'INSERT INTO reviews(review,rating,user_id,movie_id) VALUES (?,?,?,?)'
+    pool.query(sql, [review, rating, userId, movieId], (error, data) => {
+        res.send(result.createResult(error, data));
+    })
+})
+
+router.get('/myReviews', (req, res) => {
+    const { userId } = req.body;
+
+    const sql = 'SELECT title,rating,review,modified from reviews,movies WHERE  user_id=? AND reviews.movie_id = movies.id'
+    pool.query(sql, [userId], (error, data) => {
+        res.send(result.createResult(error, data));
     })
 })
 
 
 
-module.exports=router
+
+
+module.exports = router
