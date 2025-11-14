a1b47a8d659ca4f27a2580434b0c5831642a74a8
Vaishnavi Kalhapure <vaishnavi02kalhapure@gmail.com>
2025-11-14 10:58:39 +0530

    movieRouter added , multer installed

---
+++ Server/routes/movie.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const pool = require('../db/db');
+const router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'images' });
+
+router.post('/add', upload.single('img'),(req, res) => {
+    const {title,description,releaseDate}=req.body;
+})
+
+module.exports = router
--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const pool = require('./db/db');
+
+
+//user defined module
+const moviesRouter= require('./routes/movie')
+
+//creating the express object
 const app = express();
+
 
 //Middleware
 app.use(cors())
 app.use(express.json())
 app.use(express.static('images'))
 app.use(express.json())
-app.use(authorization)
 
-app.use('/user', userRouter)
+app.use('/movie', moviesRouter)
 
 
 //Server start
